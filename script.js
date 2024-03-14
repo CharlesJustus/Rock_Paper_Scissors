0bbd6e8245cd7b534c7fe5494efc6e8281d9bd9f
Charles Justus <CharleswJustus04@gmail.com>
2024-03-14 18:58:03 +0000

    begin rock paper scissors

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice(){
+    let min = Math.ceil(1);
+    let max = Math.floor(3);
+    let result = Math.floor(math.random() *(max - min + 1)) + 1;
+}
+
+getComputerChoice; 
+console.log(result);
